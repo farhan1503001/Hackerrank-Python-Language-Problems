1b09f48afba9c3256e991fb9779030bb70971c1d
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-09-14 19:57:33 +0600

    longbreakafter

---
+++ Leetcodeproblemsolve/Basic/FirstMissingpositive.py
@@ -0,0 +1,9 @@
+
+from typing import List
+
+
+class solution():
+    def firstMissingPositive(self,nums:List[int])->int:
+        """
+        target is to bring all values to it's index position
+        """
