ea56d70123fef8b5eaa660eb082a6beae281d3e1
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-12-15 19:42:02 +0600

    subsetbacktrack

---
+++ Leetcodeproblemsolve/backtracking/subsets.py
@@ -0,0 +1,24 @@
+from typing import List
+class solution():
+    answer=list()
+    def backtrack(self,first:int,combination:List[int],nums:List[int],k:int)->None:
+        if len(combination)>k:
+            return
+        if len(combination)==k:
+            self.answer.append(combination.copy())
+        
+        for i in range(first,len(nums)):
+            combination.append(nums[i])
+            self.backtrack(i+1,combination,nums,k)
+            combination.pop()
+            
+        
+    def subsets(self,nums:List[int])->List[List[int]]:
+        for index in range(0,len(nums)+1):
+            self.backtrack(0,list(),nums,index)
+        return self.answer
+    
+if __name__=='__main__':
+    ans=solution().subsets([1,2,4])
+    print(ans)
+            
