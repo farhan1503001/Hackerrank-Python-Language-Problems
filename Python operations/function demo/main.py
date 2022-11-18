1361f465f184e7985f99541fad2dd92f787cff07
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2022-11-18 20:37:24 +0600

    addition

--- Python operations/function demo/main.py
+++ Python operations/function demo/main.py
@@ -1,1 +1,11 @@
-from  
+from Function_file import *
+
+number=int(input("Enter the number: "))
+result=factorial(number)
+num=int(input("Enter the number: "))
+result=factorial(number)
+lister=[14,5,33,6,22,90]
+print(result)
+result=maximum_finder(lister)
+print(result)
+ 
